7fa55792eb41b9729b3ca19603ee0730240b410f
Breno-Souza <80594094+Breno-Souza@users.noreply.github.com>
2021-05-15 11:58:50 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const anodenacimento  = prompt('Qual ano em que vc naceu?')
+const ano = '2021'
+const nome = prompt('Qual o seu nome?')
+
+alert (`Olá, seu nome é ${nome} e você tem ${ano - anodenacimento} anos`)
